547c0fae128c958f270598290ec37e841929cf92
rokofler <rokofler@0e7c63f4-1b1c-4bf0-acd9-d4b6b27dfcb0>
2012-05-08 14:12:49 +0000

    Finished script for creating uniformly distributed additive SNP effects ro

--- trunk/scripts/PyModules/Utility.py
+++ trunk/scripts/PyModules/Utility.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python
-
+import random
 
 class File:
 	
 	@classmethod
-	def linecount(inputFile):
+	def linecount(cls,inputFile):
 		count=0;
 		for line in open(inputFile):
 			count+=1
 		return count
+
+	@classmethod
+	def randomly_draw_lines(cls,inputFile, toDraw):
+		remainingfilesize=float(File.linecount(inputFile))
+		drawn=[]
+		for line in open(inputFile):
+			threshold=float(toDraw)/remainingfilesize;
+			remainingfilesize-=1.0;	
+			if(toDraw>0 and random.random() < threshold): # needs to be < threshold! in case of <=threshold it is possible to succeed when zero more are needed (when threshold=0)
+				drawn.append(line)
+				toDraw-=1
+		return drawn
+
+			
+		
+		
+		
--- trunk/scripts/create-additive-random-uniform.py
+++ trunk/scripts/create-additive-random-uniform.py
@@ -2,6 +2,8 @@ import sys
 import random
 import collections
 from PyModules.Sync import SyncReader
+from PyModules.Utility import File
+from PyModules.Haplotype import HaplotypeIO
 from optparse import OptionParser, OptionGroup
 import re
 
@@ -9,11 +11,32 @@ import re
 # Author: Dr. Robert Kofler
 
 parser = OptionParser()
-parser.add_option("--haplotypes",dest="haplotype",help="The haplotype file")
-parser.add_option("--s",dest="s", help="the selection coefficient s")
-parser.add_option("--h",dest="h", help="the heterozygous effect")
-parser.add_option("--snp-count", dest="snp-number",help="The number of SNPs for which random additive effects should be generated")
-parser.add_option("--output",dest="output",help="the output file")
+parser.add_option("--haplotype-file",	dest="haplotype",help="The haplotype file")
+parser.add_option("--s",		dest="s", help="the selection coefficient s")
+parser.add_option("--h",		dest="h", help="the heterozygous effect")
+parser.add_option("--snp-count", 	dest="snpnumber",help="The number of SNPs for which random additive effects should be generated")
+parser.add_option("--output",		dest="output",help="the output file")
 (options, args) = parser.parse_args()
+haplotypefile=options.haplotype
 output = options.output
-popsize=int(options.popsize)
+snpcount=int(options.snpnumber)
+heteffect=float(options.h)
+selcoefficient=float(options.s)
+
+
+
+randgentypes=HaplotypeIO.parseLines(File.randomly_draw_lines(haplotypefile,snpcount))
+assert(len(randgentypes)==snpcount)
+ofh=open(output,"w")
+
+for gt in randgentypes:
+	topr=[]
+	topr.append(gt.chrom)
+	topr.append(str(gt.position))
+	if(random.random()<0.5):
+		topr.append(gt.major)
+	else:
+		topr.append(gt.minor)
+	topr.append(str(selcoefficient))
+	topr.append(str(heteffect))
+	ofh.write("\t".join(topr)+"\n")
