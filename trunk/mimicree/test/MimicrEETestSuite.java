33a5e11776588708278fea57ea1692998df85ecc
rokofler <rokofler@0e7c63f4-1b1c-4bf0-acd9-d4b6b27dfcb0>
2012-05-18 09:52:25 +0000

    finished testing of BitArray ro

---
+++ trunk/mimicree/test/MimicrEETestSuite.java
@@ -0,0 +1,14 @@
+package mimicree.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author robertkofler
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({mimicree.test.data.DataTestSuite.class})
+public class MimicrEETestSuite {
+
+}
