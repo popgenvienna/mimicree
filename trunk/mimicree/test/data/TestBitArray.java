90ef28aa2eca62fedc59f4c6de84748967ec36b9
rokofler <rokofler@0e7c63f4-1b1c-4bf0-acd9-d4b6b27dfcb0>
2012-05-18 08:45:07 +0000

    reorganizing unit-test ro

---
+++ trunk/mimicree/test/data/TestBitArray.java
@@ -0,0 +1,14 @@
+package mimicree.test.data;
+
+import static org.junit.Assert.*;
+import mimicree.data.BitArray.*;
+import org.junit.Test;
+
+public class TestBitArray {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
