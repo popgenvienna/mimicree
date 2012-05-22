f1e4d782eb0e34887db8677a7dd7d1206fa353c6
Robert Kofler <rokofler@gmail.com>
2012-05-22 11:50:59 +0000

    finished testing additiveSNP ro

--- mimicree/test/data/DataTestSuite.java
+++ mimicree/test/data/DataTestSuite.java
@@ -11,6 +11,6 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({TestBitArrayBuilder.class,TestBitArray.class,TestChromosome.class,TestSNPCollection.class,TestRandomAssortment.class
-	,TestRecombinationEventSNP.class, TestRecombinationWindow.class})
+	,TestRecombinationEventSNP.class, TestRecombinationWindow.class,TestAdditiveSNP.class})
 public class DataTestSuite {
 }
