4684dfefdd51ad8e78eb1ff4487e858855845452
rokofler <rokofler@0e7c63f4-1b1c-4bf0-acd9-d4b6b27dfcb0>
2012-05-23 09:54:35 +0000

    testes AdditiveSNPFitness ro

--- trunk/mimicree/test/data/DataTestSuite.java
+++ trunk/mimicree/test/data/DataTestSuite.java
@@ -11,6 +11,6 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({TestBitArrayBuilder.class,TestBitArray.class,TestChromosome.class,TestSNPCollection.class,TestRandomAssortment.class
-	,TestRecombinationEventSNP.class, TestRecombinationWindow.class,TestAdditiveSNP.class})
+	,TestRecombinationEventSNP.class, TestRecombinationWindow.class,TestAdditiveSNP.class, TestAdditiveSNPFitness.class})
 public class DataTestSuite {
 }
