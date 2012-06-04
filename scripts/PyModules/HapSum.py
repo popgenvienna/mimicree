7f057ea7e11f5cbc63b7e6eaffb5ae8227ad9a2d
Robert Kofler <rokofler@gmail.com>
2012-06-04 14:51:02 +0000

    updated fixed-SNP-statistic with new feature: correctly fixed ro

--- scripts/PyModules/HapSum.py
+++ scripts/PyModules/HapSum.py
@@ -2,6 +2,7 @@
 
 import sys
 import collections
+import math
 
 
 class SNPshot:
@@ -43,28 +44,71 @@ class SNPshot:
 		if(self.__major == 0 or self.__minor==0):
 			return True
 		return False
+	
+	@property
+	def isMajorFixed(self):
+		if(self.__major==self.__coverage):
+			return True
+		return False
+	
+	@property
+	def isMinorFixed(self):
+		if(self.__minor==self.__coverage):
+			return True
+		return False
 
 
 
 class SNPSuccession:
 	
 	#3R      13094319        A       A/G     G:-0.1:0.5              250:250 216:284 287:213 407:93  422:78  464:36  469:31  482:18  481:19  492:8   492:8
-	def __init__(self,chromosome, position, refChar, majorAllele,minorAllele, selected,s,h,snpshots):
+	def __init__(self,chromosome, position, refChar, majorAllele,minorAllele, w11,s,h,snpshots):
 		self.chr = chromosome
 		self.pos   = position
 		self.refChar    = refChar
 		self.majorAllele= majorAllele
 		self.minorAllele= minorAllele
-		self.selected   = selected
+		self.w11   = w11
 		self.s = s
 		self.h = h
 		self.snpshots   = snpshots
 		
 	@property
 	def isSelected(self):
-		if(self.selected is None):
+		if(self.w11 is None):
 			return False
 		return True
+	
+
+	@property
+	def posSelectedAllele(self):
+		"""
+		Return the positively selected allele
+		"""
+		if(not self.isSelected or math.fabs(self.s)<0.00000000001):
+			return None
+		if(self.majorAllele==self.w11):
+			if(self.s < 0.0):
+				# A/G   A:-.1 -> A=1.0 G=1.1
+				return self.minorAllele
+			else:
+				# A/G  A:0.1 -> A=1.0 G=0.9
+				return self.majorAllele
+		elif(self.minorAllele==self.w11):
+			if(self.s < 0.0):
+				# A/G G:-.1 -> G=1.0 A=1.1
+				return  self.majorAllele
+			else:
+				return self.minorAllele
+		else:
+			raise ValueError("impossible state")
+	
+	@property
+	def isMajorSelected(self):
+		posAllele=self.posSelectedAllele
+		if(posAllele==self.majorAllele):
+			return True
+		return False
 
 
 class SumReader:
--- scripts/analysis/fixed-SNP-stat.py
+++ scripts/analysis/fixed-SNP-stat.py
@@ -13,27 +13,40 @@ parser.add_option("--test",action="store_true", dest="test",help="run the doctes
 (options, args) = parser.parse_args()
 
 
-stat=collections.defaultdict(lambda : {'selected':0, 'nonselected':0})
+keyset=set([])
+stat=collections.defaultdict(lambda : {'selected':0,'selcorrect':0, 'nonselected':0})
 # popNumber -> selected/noneselected: 0 
 for s in SumReader(options.sum):
-	selkey="nonselected"
-	if(s.isSelected):
-		selkey="selected"
-	
 	snpshots=s.snpshots
+	selected=s.isSelected
+	isMajorSelected=s.isMajorSelected
 	for i,ss in enumerate(snpshots):
+		keyset.add(i)
 		if(ss.isFixed):
-			stat[i][selkey]+=1
+			if(selected):
+				stat[i]['selected']+=1
+				correct=False
+				if(isMajorSelected):
+					if(ss.isMajorFixed):
+						correct=True
+				else:
+					if(ss.isMinorFixed):
+						correct=True
+				if(correct):
+					stat[i]['selcorrect']+=1
+			else:
+				stat[i]['nonselected']
 
 
-keys=sorted(stat.keys())
+keys=sorted(list(keyset))
 
 
 for k in keys:
 	t=stat[k]
 	cS=t["selected"]
+	cScor=t['selcorrect']
 	cNS=t["nonselected"]
-	print("{0}\t{1}\t{2}".format(k,cS,cNS))
+	print("{0}\t{1}\t{2}\t{3}".format(k,cS,cScor,cNS))
 	
 		
 	
