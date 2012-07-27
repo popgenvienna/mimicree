414d4cd7893e2fb91dedb57aa13cb28b61339e48
Robert Kofler <rokofler@gmail.com>
2012-07-27 13:45:12 +0000

    implemented Kosambi function for recombination rate finally a usefull solution! ro

--- mimicree/test/data/TestRecombinationWindow.java
+++ mimicree/test/data/TestRecombinationWindow.java
@@ -9,6 +9,21 @@ import org.junit.Test;
 
 public class TestRecombinationWindow {
 
+	
+	@Test
+	public void test_private() {
+		RecombinationWindow rw=new RecombinationWindow(Chromosome.getChromosome("2L"),1,1000000,45);
+		
+		int count=0;
+		for(int i=0; i<10000; i++)
+		{
+			if(rw.hasRecombinationEvent()) count++;
+		}
+		
+		double bla=count;
+
+	}
+	
 	@Test
 	public void test_none() {
 		RecombinationWindow rw=new RecombinationWindow(Chromosome.getChromosome("2L"),1,1000000,0);
@@ -33,6 +48,9 @@ public class TestRecombinationWindow {
 			assertTrue(p.position()>=10);
 			assertTrue(p.position()<=15);
 		}
+		
+		
+		
 	}
 
 }
