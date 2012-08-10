ec2d3ab7bdd7481e238d828d588746b976cb7460
Robert Kofler <rokofler@gmail.com>
2012-08-10 11:13:29 +0000

    small bugfix ro

--- mimicree/analysis/rsquaredrandom/RsquaredRandomParser.java
+++ mimicree/analysis/rsquaredrandom/RsquaredRandomParser.java
@@ -55,7 +55,8 @@ public class RsquaredRandomParser {
 		sb.append("mode rsquared: calculates pairwise linkage disequilibrium between SNPs\n");
 		sb.append("--haplotypes					the haplotypes\n");
 		sb.append("--output					the output file\n");
-		sb.append("--max-distance					the maximum distance between two SNPs for calculating R^2\n");
+		sb.append("--random-samples					the number of random pairs of SNPs to sample\n");
+		sb.append("--intra-chromosomal				flag; sample random pairs of SNPs only on the same chromosome; default=inter-chromosomal\n");
 		sb.append(MimicrEE.getGeneralHelpmessage());
 		System.out.print(sb.toString());
 		System.exit(1);
