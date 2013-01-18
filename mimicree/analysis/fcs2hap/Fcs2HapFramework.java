5ca5a3689da46488259e4c5492fb17c2edc5f40e
Robert Kofler <rokofler@gmail.com>
2013-01-18 10:02:04 +0000

    novel converter started Fastcoalsim to Haplotype format

---
+++ mimicree/analysis/fcs2hap/Fcs2HapFramework.java
@@ -0,0 +1,11 @@
+package mimicree.analysis.fcs2hap;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: robertkofler
+ * Date: 1/18/13
+ * Time: 11:00 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Fcs2HapFramework {
+}
