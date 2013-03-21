1da44e434dd9f93a977b5fe1f8260202e453f98a
Robert Kofler <rokofler@gmail.com>
2013-03-21 15:47:42 +0000

    ok pieces falling into places :)

--- MimicrEELimits/src/simulate/SingleSimulationResults.java
+++ MimicrEELimits/src/simulate/SingleSimulationResults.java
@@ -1,5 +1,9 @@
 package simulate;
 
+import mimcore.data.statistic.MatingDistribution;
+
+import java.util.ArrayList;
+
 /**
  * Created with IntelliJ IDEA.
  * User: robertkofler
@@ -8,6 +12,43 @@ package simulate;
  * To change this template use File | Settings | File Templates.
  */
 public class SingleSimulationResults {
+
+	private final int fixSelection;
+	private final int fixNeutral;
+	private final int totalSNPNumber;
+	private final ArrayList<MatingDistribution> selectionMatings;
+	private final ArrayList<MatingDistribution> neutralMatings;
+	public SingleSimulationResults(ArrayList<MatingDistribution> selectionMatings, ArrayList<MatingDistribution> neutralMatings, int totalSNPNumber, int fixSelection, int fixNeutral)
+	{
+		this.selectionMatings=new ArrayList<MatingDistribution>(selectionMatings);
+		this.neutralMatings=new ArrayList<MatingDistribution>(neutralMatings);
+		this.totalSNPNumber=totalSNPNumber;
+		this.fixNeutral=fixNeutral;
+		this.fixSelection=fixSelection;
+
+	}
+
+	public int getFixSelection()
+	{return this.fixSelection;}
+
+	public int getFixNeutral()
+	{return this.fixNeutral;}
+
+	public int getTotalSNPNumber()
+	{return this.totalSNPNumber;}
+
+	public ArrayList<MatingDistribution> getNeutralMatings()
+	{
+		return new ArrayList<MatingDistribution>(this.neutralMatings);
+	}
+
+	public ArrayList<MatingDistribution> getSelectionMatingsMatings()
+	{
+		return new ArrayList<MatingDistribution>(this.selectionMatings);
+	}
+
+
+
 
 
 }
